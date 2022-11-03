1e62d51f5fc71a954362dcadc4ff6b4d7a72ee51
farhannovaldi <112691854+farhannovaldi@users.noreply.github.com>
2022-11-03 22:17:41 +0700

    Add files via upload

---
+++ jobsheet10/src/js10/Manusia.java
@@ -0,0 +1,44 @@
+package js10;
+
+/*
+ * Farhan Novaldi
+ * 21343024
+ * Informatika
+ */
+/**
+ * @author Farhan_Nvl
+ */
+import java.util.Scanner;
+
+public class Manusia {
+    String nama, tempat_lahir, tgl_lahir;
+    int berat_badan, tinggi_badan;
+
+    public void inputdata() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Masukkan nama lengkap\t: ");
+        nama = input.nextLine();
+        System.out.println("Masukkan tempat lahir\t: ");
+        tempat_lahir = input.nextLine();
+        System.out.println("Masukkan tanggal lahir\t: ");
+        tgl_lahir = input.nextLine();
+        System.out.println("Masukkan berat badan\t: ");
+        berat_badan = input.nextInt();
+        System.out.println("Masukkan tinggi badan\t: ");
+        tinggi_badan = input.nextInt();
+    }
+
+    public void identitas() {
+        System.out.println("Nama saya\t" + nama);
+        System.out.println("Tempat lahir\t" + tempat_lahir);
+        System.out.println("Tanggal lahir\t" + tgl_lahir);
+        System.out.println("Berat Badan\t" + berat_badan + "kg");
+        System.out.println("Tanggal lahir\t" + tinggi_badan + "cm");
+    }
+
+    public static void main(String[] args) {
+        Manusia A = new Manusia();
+        A.inputdata();
+        A.identitas();
+    }
+}
---
+++ jobsheet10/src/smartphone/Main.java
@@ -0,0 +1,20 @@
+package smartphone;
+
+/*
+ * Farhan Novaldi
+ * 21343024
+ * Informatika
+ */
+/**
+ * @author Farhan_Nvl
+ */
+public class Main {
+    public static void main(String[] args) {
+        Android realme = new Android();
+
+        realme.nyala();
+        realme.panggilan();
+        realme.shutdown();
+        realme.sms();
+    }
+}
